c1019c105f89c79e078aec12393d23e9423f2e0e
Nicholas Pfleegor <ndp5030@gmail.com>
2022-04-28 16:50:00 -0400

    Main part of Studio finished

--- SpinningDiscs/CD.cs
+++ SpinningDiscs/CD.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 namespace SpinningDiscs
 {
-    public class CD
+    public class CD : DiscStorage, ISpin
     {
+        public CD(string name, int capacity, string content, string type) : base(name, capacity, content, type)
+        {
+            Name = name;
+            Capacity = capacity;
+            Content = content;
+            Type = type;
+        }
+
+        public void SpinDisk()
+        {
+            Console.WriteLine("Disk is spinning!");
+        }
         // TODO: Implement your custom interface.
 
         // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
         //  need to be declared separately. 
+
     }
 }
---
+++ SpinningDiscs/ISpin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpinningDiscs
+{
+    internal interface ISpin
+    {
+        void SpinDisk();
+    }
+}
